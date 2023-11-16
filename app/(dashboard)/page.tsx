ef92d20cb2bd177b558498d23d5b312b945eb6f6
Kujoh13 <100248145+Kujoh13@users.noreply.github.com>
2023-11-16 10:17:29 +0700

    #9 user profile done

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import NotSignedInHomePage from '@/components/home/NotSignedInHomePage/NotSignedInHomePage';
 import SignedInHomePage from '@/components/home/SignedInHomePage';
+import Profile from '@/app/(dashboard)/settings/profile/[studentId]/page';
 import { authSelector } from '@/redux/auth/authSelector';
 import React from 'react'
 import { useSelector } from 'react-redux';
 
 const HomePage: React.FC = () => {
   const authState = useSelector(authSelector);
+  return <Profile/>;
   if (authState.signedIn)
     return (
       <NotSignedInHomePage/>
